bdde91671d8094f5c9fed53b4425c78e1f6565c3
Yasunao Takano <tyasunao@users.noreply.github.com>
2026-06-27 16:36:01 +0900

    Updatre LexYaccKernel

--- genimg/LexYaccKernel.py
+++ genimg/LexYaccKernel.py
@@ -1,68 +1,10 @@
-from queue import Queue
-from threading import Thread
-
 from ipykernel.kernelbase import Kernel
 import subprocess
 import tempfile
 import pexpect
 import shutil
-
-class RealTimeSubprocess(subprocess.Popen):
-    """
-    A subprocess that allows to read its stdout and stderr in real time
-    """
-
-    def __init__(self, cmd, write_to_stdout, write_to_stderr):
-        """
-        :param cmd: the command to execute
-        :param write_to_stdout: a callable that will be called with chunks of data from stdout
-        :param write_to_stderr: a callable that will be called with chunks of data from stderr
-        """
-        self._write_to_stdout = write_to_stdout
-        self._write_to_stderr = write_to_stderr
-
-        super().__init__(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, bufsize=0)
-
-        self._stdout_queue = Queue()
-        self._stdout_thread = Thread(target=RealTimeSubprocess._enqueue_output, args=(self.stdout, self._stdout_queue))
-        self._stdout_thread.daemon = True
-        self._stdout_thread.start()
-
-        self._stderr_queue = Queue()
-        self._stderr_thread = Thread(target=RealTimeSubprocess._enqueue_output, args=(self.stderr, self._stderr_queue))
-        self._stderr_thread.daemon = True
-        self._stderr_thread.start()
-
-    @staticmethod
-    def _enqueue_output(stream, queue):
-        """
-        Add chunks of data from a stream to a queue until the stream is empty.
-        """
-        for line in iter(lambda: stream.read(4096), b''):
-            queue.put(line)
-        stream.close()
-
-    def write_contents(self):
-        """
-        Write the available content from stdin and stderr where specified when the instance was created
-        :return:
-        """
-
-        def read_all_from_queue(queue):
-            res = b''
-            size = queue.qsize()
-            while size != 0:
-                res += queue.get_nowait()
-                size -= 1
-            return res
-
-        stdout_contents = read_all_from_queue(self._stdout_queue)
-        if stdout_contents:
-            self._write_to_stdout(stdout_contents)
-        stderr_contents = read_all_from_queue(self._stderr_queue)
-        if stderr_contents:
-            self._write_to_stderr(stderr_contents)
-
+import shlex
+import traceback
 
 class LexYaccKernel(Kernel):
     implementation = 'lex yacc'
@@ -88,6 +30,8 @@ class LexYaccKernel(Kernel):
     #        os.remove(file)
     #     os.remove(self.master_path)
 
+
+
     def new_temp_file(self, **kwargs):
         """Create a new temp file to be deleted when the kernel shuts down"""
         # We don't want the file to be deleted when closed, but only when the kernel stops
@@ -97,37 +41,119 @@ class LexYaccKernel(Kernel):
         self.files.append(file.name)
         return file
 
+    def _to_text(self, contents):
+        if isinstance(contents, bytes):
+            return contents.decode('utf-8', errors='replace')
+        return str(contents)
+
     def _write_to_stdout(self, contents):
-        self.send_response(self.iopub_socket, 'stream', {'name': 'stdout', 'text': contents})
+        self.send_response(
+            self.iopub_socket,
+            'stream',
+            {'name': 'stdout', 'text': self._to_text(contents)}
+        )
 
     def _write_to_stderr(self, contents):
-        self.send_response(self.iopub_socket, 'stream', {'name': 'stderr', 'text': contents})
+        self.send_response(
+            self.iopub_socket,
+            'stream',
+            {'name': 'stderr', 'text': self._to_text(contents)}
+        )
 
     def create_jupyter_subprocess(self, cmd):
-        return RealTimeSubprocess(cmd,
-                                  lambda contents: self._write_to_stdout(contents.decode()),
-                                  lambda contents: self._write_to_stderr(contents.decode()))
+        """Run a command, then send the complete stdout/stderr to the notebook."""
+        try:
+            result = subprocess.run(
+                cmd,
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE,
+                text=True,
+                encoding='utf-8',
+                errors='replace',
+            )
+        except FileNotFoundError:
+            result = subprocess.CompletedProcess(
+                cmd,
+                127,
+                '',
+                '[Kernel] command not found: {}\n'.format(cmd[0]),
+            )
+        except Exception as e:
+            result = subprocess.CompletedProcess(
+                cmd,
+                1,
+                '',
+                '[Kernel] failed to run {}: {}\n'.format(shlex.join(cmd), e),
+            )
 
-    def compile_with_lex(self, source_filename, options=[]):
+        if result.stdout:
+            self._write_to_stdout(result.stdout)
+        if result.stderr:
+            self._write_to_stderr(result.stderr)
+
+        return result
+
+    def compile_with_lex(self, source_filename, options=None):
+        options = options or []
         args = ['lex'] + options + [source_filename]
         return self.create_jupyter_subprocess(args)
     
-    def compile_with_yacc(self, source_filename, options=[]):
-        args = ['yacc', source_filename, "-Wcounterexamples"] + options
+    def compile_with_yacc(self, source_filename, options=None):
+        options = options or []
+        args = ['yacc', '-Wcounterexamples'] + options + [source_filename]
         return self.create_jupyter_subprocess(args)
 
-    def compile_with_gcc(self, source_filename, options=[]):
+    def compile_with_gcc(self, source_filename, options=None):
+        options = options or []
         args = ['gcc', source_filename] + options + ['-w', '-ll', '-ly']
         return self.create_jupyter_subprocess(args)
+
     def compile_asm(self, source_filename):
         args = ['cc', '-no-pie', '-z', 'execstack', source_filename]
         return self.create_jupyter_subprocess(args)
 
+    def _ok(self):
+        return {
+            'status': 'ok',
+            'execution_count': self.execution_count,
+            'payload': [],
+            'user_expressions': {}
+        }
+
+    def _error(self, ename, evalue, traceback_lines=None):
+        return {
+            'status': 'error',
+            'execution_count': self.execution_count,
+            'ename': ename,
+            'evalue': evalue,
+            'traceback': traceback_lines or [evalue],
+        }
 
     def do_execute(self, code, silent, store_history=True,
-                    user_expressions=None, allow_stdin=False):
-
+                   user_expressions=None, allow_stdin=False):
+        try:
+            return self._do_execute_impl(
+                code,
+                silent,
+                store_history,
+                user_expressions,
+                allow_stdin,
+            )
+        except Exception:
+            tb = traceback.format_exc()
+            self._write_to_stderr('[Kernel] internal error:\n' + tb)
+            return self._error(
+                'KernelInternalError',
+                'Unhandled exception in LexYaccKernel',
+                tb.splitlines(),
+            )
+            
+    def _do_execute_impl(self, code, silent, store_history=True,
+                         user_expressions=None, allow_stdin=False):
         lines = code.splitlines()
+        if not lines:
+            return self._ok()
+        
         if lines[0].startswith('/*') and lines[0].endswith('*/'):
             command = lines[0].split()[1]
             if command == 'lex':
@@ -138,12 +164,10 @@ class LexYaccKernel(Kernel):
                     f.write('\n'.join(lines[1:]) + '\n')
                     f.flush()
                     p = self.compile_with_lex(f.name, options)
-                    while p.poll() is None:
-                        p.write_contents()
                     if p.returncode != 0:
-                        self._write_to_stderr("[Lex] flex exited with code {}".format(p.returncode))
+                        self._write_to_stderr("[Lex] flex exited with code {}\n".format(p.returncode))
                     else:
-                        self._write_to_stdout("[Lex] flex generates lex.yy.c successfully")
+                        self._write_to_stdout("[Lex] flex generates lex.yy.c successfully\n")
             elif command == 'yacc':
                 filename = lines[0].split()[2]
                 options = lines[0].split()[3:-1]
@@ -151,12 +175,10 @@ class LexYaccKernel(Kernel):
                     f.write('\n'.join(lines[1:]) + '\n')
                     f.flush()
                     p = self.compile_with_yacc(f.name, options)
-                    while p.poll() is None:
-                        p.write_contents()
                     if p.returncode != 0:
-                        self._write_to_stderr("[Yacc] bison exited with code {}".format(p.returncode))
+                        self._write_to_stderr("[Yacc] bison exited with code {}\n".format(p.returncode))
                     else:
-                        self._write_to_stdout("[Yacc] bison generates y.tab.c successfully")
+                        self._write_to_stdout("[Yacc] bison generates y.tab.c successfully\n")
             elif command == 'c':
                 filename = lines[0].split()[2]
                 options = lines[0].split()[3:-1]
@@ -164,12 +186,10 @@ class LexYaccKernel(Kernel):
                     f.write('\n'.join(lines[1:]) + '\n')
                     f.flush()
                     p = self.compile_with_gcc(f.name, options)
-                    while p.poll() is None:
-                        p.write_contents()
                     if p.returncode != 0:
-                        self._write_to_stderr("[C] gcc exited with code {}".format(p.returncode))
+                        self._write_to_stderr("[C] gcc exited with code {}\n".format(p.returncode))
                     else:
-                        self._write_to_stdout("[C] gcc generates a.out successfully")
+                        self._write_to_stdout("[C] gcc generates a.out successfully\n")
             elif command == 'a.out':
                 try :
                     p = pexpect.spawn('./a.out')
@@ -232,12 +252,10 @@ class LexYaccKernel(Kernel):
                         f2.write('\n'.join(diff))
                     with open(filename + '.s', 'r') as f2:
                         p2 = self.compile_asm(filename + '.s')
-                        while p2.poll() is None:
-                            p2.write_contents()
                         if p2.returncode != 0:
-                            self._write_to_stderr("[UECC] cc exited with code {}".format(p2.returncode))
+                            self._write_to_stderr("[UECC] cc exited with code {}\n".format(p2.returncode))
                         else:
-                            self._write_to_stdout("[UECC] cc generates a.out successfully")
+                            self._write_to_stdout("[UECC] cc generates a.out successfully\n")
             else:
                 self._write_to_stderr("[Kernel] Error: The code must be start with /* and the format is [/* (lex|yacc|c|uecc) filename */] or [/* a.out */]")
         else:
